54a78736cac3dfc26ad9cd21d65c36117da0ba3d
Md Ala Uddin <92620825+mdalauddin45@users.noreply.github.com>
2024-11-07 10:46:01 +0600

    all done

--- problem-8.ts
+++ problem-8.ts
@@ -1,10 +1,10 @@
 {
   //
   function validateKeys<T extends object>(obj: T, keys: (keyof T)[]): boolean {
-    let flag=false;
-    keys.forEach(key => {
-      if(key in obj) flag = true;
-    })
+    let flag = false;
+    keys.forEach((key) => {
+      if (key in obj) flag = true;
+    });
     return flag;
   }
   const person = {
